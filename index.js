e03eaf7ca4e4841ef7979b5edc49991dc115566f
Nicholas MacDonald <macdonaldn@ufl.edu>
2022-08-27 16:00:06 -0400

    Finished Greeting System

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function greet() {
+    var name = prompt("What is your name?")
+    alert("Hello, " + name)
+    var age = parseInt(prompt("How old are you?"))
+    var bday = confirm("Have you already had your birthday this year?")
+    var thisYear = new Date()
+    var birthYear
+    if (bday) {
+        birthYear = thisYear.getFullYear() - age
+    }else{
+        birthYear = thisYear.getFullYear() - (age+1)
+    }
+    alert ("You were born on " + birthYear)
+}
